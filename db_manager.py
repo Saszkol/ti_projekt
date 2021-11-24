459430ea2701d69d9b4cdb663c180e6e36738721
saszkol <m.suchecki@ms.wwsi.edu.pl>
2021-11-24 21:37:05 +0100

    Added insert command to db manager

--- db_manager.py
+++ db_manager.py
@@ -6,7 +6,13 @@ cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
 
 
 cursor = cnxn.cursor()
-cursor.execute('SELECT * FROM dbo.test')
-
+cursor.execute('SELECT * FROM dbo.Users')
 for row in cursor:
     print('row = %r' % (row,))
+
+
+
+
+
+
+# cursor.execute("insert into Users (ID_User, Imie, Nazwisko, Numer_Telefonu, Email) values (5, 'Zephania','Harrison', '123456789','leo@mattisCraseget.com')")
